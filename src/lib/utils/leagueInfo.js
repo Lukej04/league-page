fdded3cc96b8bb10dc6433f2bb7674ff7aa3a747
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2024-11-05 18:36:41 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -171,10 +171,10 @@ export const managers = [
       "managerID": "995881230036000768",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Tanner Clary",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "?", // (optional)
+      "location": "San Francisco", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Luke Johnson", // Can be anything (usually your rival's name)
