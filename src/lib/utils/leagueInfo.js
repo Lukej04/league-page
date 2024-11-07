6bdae3803db9b8b217453dc071366040af225266
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2024-11-06 20:08:22 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -89,7 +89,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Columbia", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "Blake.jpeg", // square ratio recommended (no larger than 500x500)
+      "photo": "Blake.png", // square ratio recommended (no larger than 500x500)
       "favoriteTeam": "car", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
