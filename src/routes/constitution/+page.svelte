9c7560f7f86020eab120ee37171dd9628c18edae
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2024-11-11 11:26:05 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { dues } from '$lib/utils/helper';
     let one, oneOne;
-    let two, twoOne, twoTwo, twoTwoOne, twoTwoTwo, twoTwoThree, twoTwoFour, twoTwoFive, twoThree;
+    let two, twoOne, twoTwo, twoTwoOne, twoTwoTwo, twoTwoThree, twoTwoFour, twoThree;
     let three, threeOne, threeTwo, threeThree, threeFour;
     let four, fourOne;
     let five, fiveOne;
@@ -113,7 +113,6 @@
             <h5 class="noUnderscore clickable" on:click={() => goToSection(twoTwoTwo)}>2.2.2 Lending Players Prohibition</h5>
             <h5 class="noUnderscore clickable" on:click={() => goToSection(twoTwoThree)}>2.2.3 Trade Deadline</h5>
             <h5 class="noUnderscore clickable" on:click={() => goToSection(twoTwoFour)}>2.2.4 Trade Restrictions</h5>
-            <h5 class="noUnderscore clickable" on:click={() => goToSection(twoTwoFive)}>2.2.5 Veto/Trade Process</h5>
         <h4 class="noUnderscore clickable" on:click={() => goToSection(twoThree)}>2.3 Waiver Wire</h4>
     
     <h3 class="noUnderscore clickable" on:click={() => goToSection(three)}>Section 3: Drafting</h3>
@@ -162,7 +161,7 @@
     <h2 class="sectionHeading" bind:this={one}>Section 1 League Governance</h2>
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Commissioners</h3>
-    <p>The Fantasy Girdiron Dynasty League is a 10 team league run though the Sleeper app. The league will be governed by a board of 3 commissioners (Matty Jorgensen, Trace Pigg & Luke Johnson). Commissioner duties include enforcing rules, managing league finances, facilitating offseason activities, and adjudicating disputes. Commissioners will serve indefinitely until self-demotion or removal from the legue.</p>
+    <p>The Fantasy Girdiron Dynasty League is a 10 team league run though the Sleeper app. The league will be governed by a board of 3 commissioners (Matty Jorgensen, Trace Pigg & Luke Johnson). Commissioners duties include enforcing rules, managing league finances, facilitating offseason activities, and adjudicating disputes. Commissioners will serve indefinitely until self-demotion or removal from the legue.</p>
     <p>Major league decisions (rule changes, expansion, realignment) require a 8/2 owner vote.</p>
 
     <h2 class="sectionHeading" bind:this={two}>Section 2 Roster Breakdown</h2>
@@ -197,7 +196,7 @@
     
     <h4 bind:this={twoTwoOne}>2.2.1 Trade Collusion</h4>
     <div class="subBlock">
-        <p>*If any owners are suspected of accepting bribes/offering bribes to enhance their/another owners team via trade, trading any assets other than fantasy players, draft picks, or otherwise found guilty of engaging in any form of collusion*, all parties involved in the trade will be susceptible to punishment. The punishments will go as followed:</p>
+        <p>If any owners are suspected of accepting bribes/offering bribes to enhance their/another owners team via trade, trading any assets other than fantasy players, draft picks, or otherwise found guilty of engaging in any form of collusion, all parties involved in the trade will be susceptible to punishment. The punishments will go as followed:</p>
         
         <ul>
             <li>First Offense – Immediate reduction of FAAB budget by 80%</li>
@@ -205,34 +204,28 @@
             <li>Third Offense – A league meeting of all members not involved in any of the improper trades/activity in question will take place to discuss next steps.</li>
         </ul>
 
-        <p>*All trades that are found guilty of collusion, will be reversed/denied. If the trade had affected the outcome of previous games, the results of those games will be revised.  </p>
+        <p>All trades that are found guilty of collusion, will be reversed/denied. If the trade had affected the outcome of previous games, the results of those games will be revised.  </p>
 
-        <p>*Collusion includes the organizing of veto votes against an acceptable trade.</p>
+        <p>Collusion includes the organizing of veto votes against an acceptable trade.</p>
 
-        <p>*The commissioner will err on the side of letting owners manage their teams the way they see fit. Any collusion will need to be clear. The commissioner reserves the right to step in and at the very least assess the thinking of any team involved in a heavily lop-sided trade. </p>
+        <p>The commissioners will err on the side of letting owners manage their teams the way they see fit. Any collusion will need to be clear. The commissioners reserve the right to step in and at the very least assess the thinking of any team involved in a heavily lop-sided trade. </p>
     </div>
 
     <h4 bind:this={twoTwoTwo}>2.2.2 Lending Players Prohibition</h4>
     <div class="subBlock">
-        <p>*Any player traded from a team may not be re-acquired via trade within 6 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
+        <p>Any player traded from a team may not be re-acquired via trade within 6 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
     </div>
     
     <h4 bind:this={twoTwoThree}>2.2.3 Trade Deadline</h4>
     <div class="subBlock">
-        <p>*The trade deadline will be set for the Saturday of the week 11 games of the NFL season.</p>
-        <p>*Trades can be made again after the Fantasy Playoffs are complete.</p>
+        <p>The trade deadline will be set for the Saturday of the week 11 games of the NFL season.</p>
+        <p>Trades can be made again after the Fantasy Playoffs are complete.</p>
     </div>
     
     <h4 bind:this={twoTwoFour}>2.2.4 Trade Restrictions</h4>
     <div class="subBlock">
-        <p>*You will not be able to trade draft picks that are more than 3 drafts away. For example, during the 2024 season you cannot trade any picks in or after the 2028 draft.</p>
+        <p>You will not be able to trade draft picks that are more than 3 drafts away. For example, during the 2024 season you cannot trade any picks in or after the 2028 draft.</p>
     </div>
-    
-    <h4 bind:this={twoTwoFive}>2.2.5 Veto/Trade Process</h4>
-    <div class="subBlock">
-        <p>*A total of 7 veto votes are required to veto a trade. Vetoes are only appropriate in instances of collusion or an egregious mismatch of value (i.e. CMC for a 3rd string QB). Not liking a trade does not warrant a veto.</p>
-    </div>
-    
     
     
     <h3 bind:this={twoThree}>2.3 Waiver Wire</h3>
@@ -260,7 +253,7 @@
     <p>For the teams that do not make the playoffs, their draft spots will be determined by the inverse order of max points for (Lowest total points gets 1st Overall pick).</p>
     
     <h3 bind:this={threeFour}>3.4 Draft Day Trades</h3>
-    <p>*If a trade is being made during the draft, one or both parties can alert the commissioner to pause the draft to allow for extra time to negotiate the trade.</p>
+    <p>If a trade is being made during the draft, one or both parties can alert the commissioners to pause the draft to allow for extra time to negotiate the trade.</p>
     
     <h2 class="sectionHeading" bind:this={four}>Section 4 Scoring System</h2>
 
@@ -275,25 +268,22 @@
     <h2 class="sectionHeading" bind:this={six}>Section 6 Postseason</h2>
     
     <h3 bind:this={sixOne}>6.1 Playoffs</h3>
-    <p>*At the end of the season, 6 teams will make the playoffs. The first round of playoffs will commence in week 15. The teams that finish with the best records, will receive a playoff berth. The 2 best teams will receive a bye week in the first round of the playoffs. Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
-    <p>NOTE: Playoffs Sytle is Re-Seed. The higher seeded teams always play lower-seeded teams in every round</p>
+    <p>At the end of the season, 6 teams will make the playoffs. The first round of playoffs will commence in week 15. The teams that finish with the best records, will receive a playoff berth. The 2 best teams will receive a bye week in the first round of the playoffs. Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
+    <p>NOTE: Playoffs Style is Re-Seed. The higher seeded teams always play lower-seeded teams in every round</p>
 
     <h3 bind:this={sixTwo}>6.2 Seeding Tiebreakers</h3>
-    <p>*If two teams finish with the same record the tiebreakers will go as followed:</p>
+    <p>If two teams finish with the same record the tiebreakers will go as followed:</p>
 
     <ol>
         <li>Points For</li>
-        <li>Head 2 Head Record</li>
-        <li>Division Record</li>
         <li>Total Points Against</li>
-        <li>Coin Flip</li>
-        <li>Duel to the death</li>
+        <li>Coin Flip or Duel to the death</li>
     </ol>
 
     <h2 class="sectionHeading" bind:this={seven}>Section 7 Tanking Policy</h2>
     
     <h3 bind:this={sevenOne}>7.1 League Tanking Policy</h3>
-    <p>*No team may intentionally leave starting roster spots empty. Any evidence of intentionally leaving roster spots empty in order to improve your draft position may be subject to punishment, including but not limited to the forfeiture of draft selections.</p>
+    <p>No team may intentionally leave starting roster spots empty. Any evidence of intentionally leaving roster spots empty in order to improve your draft position may be subject to punishment, including but not limited to the forfeiture of draft selections.</p>
 
     <h2 class="sectionHeading" bind:this={eight}>Section 8 Conduct and Sportsmanship</h2>
     
@@ -304,21 +294,21 @@
     <h2 class="sectionHeading" bind:this={nine}>Section 9 Replacing Managers</h2>
     
     <h3 bind:this={nineOne}>9.1 Removing Managers</h3>
-    <p>*Short of continuously failing to set a valid lineup, or confirmed collusion, no manager can be removed against their will. Participation, through the league chat, trade offers, and the waiver wire, is strongly encouraged but generally grounds for removal.</p>
+    <p>Short of continuously failing to set a valid lineup, or confirmed collusion, no manager can be removed against their will. Participation, through the league chat, trade offers, and the waiver wire, is strongly encouraged but generally not grounds for removal.</p>
     <p>Owners may be removed by a 8/2 league vote for collusion, lack of participation, or failure to pay league fees.</p>
 
     <h3 bind:this={nineTwo}>9.2 Replacing Managers</h3>
     <p>Ownership may be transferred to a new owner only with league approval.</p>
-    <p>*When a manager needs to be replaced, the commissioners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a dynasty commitment and will be very active within the league.</p>
+    <p>When a manager needs to be replaced, the commissioners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a dynasty commitment and will be very active within the league.</p>
     
     <h3 bind:this={nineThree}>9.3 Replacement Incentive</h3>
-    <p>*In the event that a manager chooses to quit the league or is removed by a comissioner, the $25 early deposit will not be returned and will transfer over to the new owner who replaces them.</p>
+    <p>In the event that a manager chooses to quit the league or is removed by a comissioner, the $25 early deposit will not be returned and will transfer over to the new owner who replaces them.</p>
 
     <h2 class="sectionHeading" bind:this={ten}>Section 10 League Finances</h2>
     
     <h3 bind:this={tenOne}>10.1 League Dues</h3>
     <p>First season league fee of $100, where $25 will be held for the following season as an early deposit.</p>
-    <p>*League dues are set at ${dues} for the all following seasons, with $25 being held as an early deposit for next season every year. Dues collection will be sent out no later than the first of August. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a 10% penalty will be added to their payment and will go towards the payouts at the end of the year.</p>
+    <p>League dues are set at ${dues} for all the following seasons, with $25 being held as an early deposit for next season every year. All managers must be fully paid before the start of the rookie draft. In the event that a manager fails to pay before the deadline, an additional $25 penalty will be added to their payment and will go towards the payouts at the end of the year.</p>
     
     <h3 bind:this={tenTwo}>10.2 Payout</h3>
     <p>League payout is structured as follows:</p>
@@ -327,9 +317,9 @@
         <li>2nd place: $100</li>
         <li>Most Points Scored in Single Regular Season Week: $75</li>
     </ul>
-    <p>*In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout.</p>
+    <p>In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout.</p>
     
     <h3 bind:this={tenThree}>10.3 Raising Dues</h3>
-    <p>*A ⅔ majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buyin, finding a co-manager to split the cost is reccomended.</p>
+    <p>A 8/2 majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buy-in, finding a co-manager to split the cost is recommended.</p>
 
 </div>
